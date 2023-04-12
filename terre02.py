c55c58ac54b71da4b224ad9c971e95ce6ead58a0
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2023-04-12 20:36:29 +0200

    Exercice3

---
+++ terre02.py
@@ -0,0 +1,8 @@
+# Creez une variable qui contient une liste avec les caracteres que vous souhaitez
+epreuve_terre = ["Je","ne","vais","pas","abandonner","!"]
+
+# Creez une boucle for qui parcours chaque caracteres puis les places ligne par ligne 
+for epreuve in epreuve_terre:
+
+# Affichez la variable creer dans la boucle
+  print(epreuve)
